e4f0a324321d25e1f7c08058af908d10fee64fb1
Shahid <69500075+Shahid718@users.noreply.github.com>
2024-08-20 00:55:47 +0900

    Update fd_dendrite_dislin_sub_v2.f90
    replace do concurrent with simple do loop

--- dendrite/subroutines/fd_dendrite_dislin_sub_v2.f90
+++ dendrite/subroutines/fd_dendrite_dislin_sub_v2.f90
@@ -180,7 +180,8 @@ contains
     real ( kind = 8 ) , intent ( in out )                      :: theta_, m_
 
 
-    do concurrent ( j=1:Ny, i=1:Nx )
+    do i = 1, Nx
+       do j = 1, Ny
 
        jp = j + 1
        jm = j - 1
@@ -215,11 +216,12 @@ contains
             & ( theta - theta0 ) ) )
        epsilon_deriv_(i,j) = -epsilonb*aniso*delta*sin &
             & ( aniso*( theta - theta0 ) )
-
+            
+       end do
     end do
 
-
-    do concurrent ( j=1:Ny, i=1:Nx )
+    do i = 1, Nx
+       do j = 1, Ny
 
        jp = j + 1
        jm = j - 1
@@ -253,6 +255,7 @@ contains
        tempr_(i,j) = tempr_(i,j) + dtime*lap_tempr_(i,j) &
             & + kappa*( phi_(i,j) - phi_old_ )
 
+       end do
     end do
 
 
