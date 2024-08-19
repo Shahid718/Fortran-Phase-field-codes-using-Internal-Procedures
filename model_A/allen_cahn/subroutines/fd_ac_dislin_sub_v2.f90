875aa016a06c073bc2b85086785ae449492dd936
Shahid <69500075+Shahid718@users.noreply.github.com>
2024-08-20 01:20:29 +0900

    Update fd_ac_dislin_sub_v2.f90
    replace do concurrent with simple do loop.

--- model_A/allen_cahn/subroutines/fd_ac_dislin_sub_v2.f90
+++ model_A/allen_cahn/subroutines/fd_ac_dislin_sub_v2.f90
@@ -139,8 +139,8 @@ contains
     integer ( kind = 4 ) :: i, j, jp, jm, ip, im, dx = 2, dy = 2
 
 
-    do concurrent ( j = 1:Ny, i = 1:Nx )
-
+    do i = 1, Nx
+       do j = 1, Ny
 
        ! free energy derivative
 
@@ -169,7 +169,7 @@ contains
 
        phi_(i,j) = phi_(i,j) - dt*mobility*dummy_phi(i,j)
 
-
+       end do
     end do
 
 
