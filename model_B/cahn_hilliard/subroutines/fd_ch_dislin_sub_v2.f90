30ef1e5b4bf4199f5b7be0c13da79edd00e6238c
Shahid <69500075+Shahid718@users.noreply.github.com>
2024-08-20 01:37:16 +0900

    Update fd_ch_dislin_sub_v2.f90
    replace do concurrent with simple do loop.

--- model_B/cahn_hilliard/subroutines/fd_ch_dislin_sub_v2.f90
+++ model_B/cahn_hilliard/subroutines/fd_ch_dislin_sub_v2.f90
@@ -138,7 +138,8 @@ contains
     integer ( kind = 4 ) :: i, j, jp, jm, ip, im, dx =1, dy = 1
 
 
-    spatial_loop: do concurrent ( i =1:Nx, j=1:Ny )
+    do i = 1, Nx
+       do j = 1, Ny
 
 
        ! free energy derivative
@@ -171,9 +172,10 @@ contains
        ! time integration
 
        con_(i,j) =  con_(i,j) + dt*mobility*lap_dummy_(i,j)
-       
 
-    end do spatial_loop
+       
+       end do
+    end do
     
 
   end subroutine Perform_evolution
